be8a4f87feee95cad2d9735f7ed06cc529a73df8
tshah-mulesoft <tarakkumar.shah@mulesoft.com>
2024-07-11 20:05:39 +0530

    Add postgres db

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 
 const PORT = process.env.PORT || 5001
+
+function showTimes() {
+  const times = process.env.TIMES || 5
+  let result = ''
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,4 +28,17 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
